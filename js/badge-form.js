d235172b15c7e27ab33397e3c8c0f5eebf4b9082
pacmanw2 <marcog3210@gmail.com>
2018-05-24 14:27:58 -0700

    added function to get the status of a hunt and js to set bad-form to READ ONLY

---
+++ js/badge-form.js
@@ -0,0 +1,4 @@
+/*sets the badge-form to read only*/
+function setToReadOnly() {
+    $('#badge-form :input').attr('readonly','readonly');
+}
---
+++ js/master.js
@@ -0,0 +1,26 @@
+/*this will get the status of a hunt.
+* Needs hunt id & token from firebase*/
+function getHuntStatus(huntId, token) {
+    $.ajax({
+        type: "GET",
+        contentType: "application/json",
+        url: "https://magpiehunt.com/api/v1/hunts/" + huntId,
+        dataType: "text",
+        timeout: 600000,
+        "headers": {
+            "Authorization": "Bearer " + token,
+        },
+        success: function (data) {
+            var temp = JSON.parse(data);
+            var status = temp["data"]["approval_status"];
+            console.log(temp["data"]["approval_status"]);
+            return status;
+
+        },
+        error: function (e) {
+            alert("Error encountered while getting the status of hunt: " + huntId);
+
+        }
+    });
+}
+
