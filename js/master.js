ab3fd296dbdc41fe3ae467f25d18cb1810db7973
Allaris <gameplayer0@gmail.com>
2018-05-24 14:37:01 -0700

    moved get url param to master

---
+++ js/master.js
@@ -0,0 +1,9 @@
+// gets the params from URL
+// example console.log(getUrlVars()["huntID"]);
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
