3f5a6bba44394f8e73b66b5d3b362f068b89e619
Allaris <gameplayer0@gmail.com>
2018-05-29 12:40:22 -0700

    badges dynamically created

--- js/review-hunt.js
+++ js/review-hunt.js
@@ -10,17 +10,14 @@ var huntSettings = {
     }
 };
 $.ajax(huntSettings).done(function (response) {
-    console.log(response);
-
     var huntJSON = JSON.parse(response);
-    console.log(huntJSON["data"]["abbreviation"]);
-    console.log(huntJSON["data"]["name"]);
-    console.log(huntJSON["data"]["summary"]);
 
     // fill in hunt info
     document.getElementById("review-hunt-abbreviation").innerText = huntJSON["data"]["abbreviation"];
     document.getElementById("review-hunt-name").innerText = huntJSON["data"]["name"];
     document.getElementById("review-hunt-description").innerText = huntJSON["data"]["summary"];
+
+    document.getElementById("review-hunt-super-badge-image-link").href = "hunt-form.html?badgeID=" + getUrlVars()["huntID"];
 });
 
 
@@ -42,10 +39,17 @@ $.ajax(awardSettings).done(function (response) {
     // remove new award box
     document.getElementById("add-new-award-btn-container").remove();
 
+    // TODO: populate award info
+    // populate existing award
+
 }).fail(function () {
-    // award does not exists
+    // remove edit award box
     document.getElementById("existing-award-container").remove();
+
+    // set link for creating new badge for new award
+    document.getElementById("add-new-award-btn").href = "award-form.html?huntID=" + getUrlVars()["huntID"];
 });
+
 
 
 // populate badges
@@ -61,9 +65,61 @@ var badgeSettings = {
 };
 $.ajax(badgeSettings).done(function (response) {
     // badges exists
-}).fail(function (){
-    // badges do not exists
+    var badgesJSON = JSON.parse(response);
 
+    // loop through json badges
+    for (var i = 0; i < badgesJSON.length; i++) {
+        console.log(badgesJSON[i]["data"]["badge_id"]);
+
+        // create current badge html
+        // TODO: set image to that of one of the badge image
+        document.getElementById("badges-container").innerHTML +=
+            "<div class=\"column is-one-quarter\">\r\n" +
+                "<p id=\"review-hunt-badge-name\" class=\"content\">" + badgesJSON[i]["data"]["landmark_name"] + "<\/p>\r\n" +
+                "<a href=\"badge-form.html?huntID=" + getUrlVars()["huntID"] + "&badgeID=" + badgesJSON[i]["data"]["badge_id"] + "\">\r\n" +
+                    "<img id=\"review-hunt-badge-image\" src=\"assets\/Magpie__Centennial Sculpture.png\">\r\n" +
+                "</a>\r\n" +
+                "<div class=\"field is-grouped\">\r\n" +
+                    "<button id=\"review-hunt-badge-view-btn\" class=\"button is-rounded btn-standard icon ion-md-eye\"><\/button>\r\n                                <button id=\"review-hunt-badge-edit-btn\" class=\"button is-rounded btn-standard icon ion-md-create\"><\/button>\r\n                                <button id=\"review-hunt-badge-delete-btn\" class=\"button is-rounded btn-standard icon ion-md-trash\"><\/button>\r\n" +
+                "<\/div>\r\n" +
+            "<\/div>"
+    }
+
+    newBadgeButton();
+}).fail(function (){
+    newBadgeButton();
 });
 
+
+// generate and add the new badge button if hunt status allows for it
+function newBadgeButton() {
+    $.ajax({
+        type: "GET",
+        contentType: "application/json",
+        url: "https://magpiehunt.com/api/v1/hunts/" + getUrlVars()["huntID"],
+        dataType: "text",
+        timeout: 600000,
+        "headers": {
+            "Authorization": "Bearer " + localStorage.getItem("token"),
+        },
+        success: function (data) {
+            var temp = JSON.parse(data);
+            var status = temp["data"]["approval_status"];
+
+            if (status === "non-approved") {
+                console.log("enabling new badge button");
+                // add new badge button
+                document.getElementById("badges-container").innerHTML +=
+                    "<div id=\"add-new-badge-btn-container\" class=\"column is-one-quarter\">\r\n" +
+                    "<a id=\"add-new-badge-btn\" href=\"badge-form.html\">+<br>ADD BADGE<\/a>\r\n" +
+                    "<\/div>"
+            } else {
+                console.log("disabling new badge button");
+            }
+        },
+        error: function (e) {
+            alert("Error encountered while getting the status of hunt: " + getUrlVars()["huntID"]);
+        }
+    });
+}
 
